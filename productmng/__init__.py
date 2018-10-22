b16766cd98fd634c086cc6ba021f8624c3aebbcd
Vu Le Nam <namvule@hotmail.com>
2018-10-22 15:24:07 +0900

    add instance_relative_config

--- productmng/__init__.py
+++ productmng/__init__.py
@@ -3,7 +3,7 @@
 from flask import Flask
 from flask_mongoengine import MongoEngine
 
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('config')
 app.config.from_pyfile('config.py')
 
