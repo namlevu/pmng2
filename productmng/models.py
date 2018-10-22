7d2bb7caef0effa1d34ada17a17789cc54c64072
Vu Le Nam <namvule@hotmail.com>
2018-10-22 15:18:29 +0900

    append model

---
+++ productmng/models.py
@@ -0,0 +1,57 @@
+import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from productmng import db
+
+
+class Photo(db.DynamicDocument):
+    url = db.StringField()
+
+
+class User(db.DynamicDocument):
+    username = db.StringField()
+    photo = db.EmbeddedDocumentField('Photo')
+    fullname = db.StringField()
+    email = db.StringField()
+    password_hash = db.StringField()
+    disabled = db.BooleanField()
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+class Cost(db.DynamicDocument):
+    amount = db.IntField()
+    reason = db.StringField()
+    created_by = db.EmbeddedDocumentField('User')
+    created_at = db.DateTimeField(default=datetime.datetime.now, required=True)
+
+class TrackingStatus(db.DynamicDocument):
+    index = db.IntField()
+    name = db.StringField()
+
+class Product(db.DynamicDocument):
+    name = db.StringField()
+    category = db.StringField()
+    brand = db.StringField()
+    photos = db.ListField(db.EmbeddedDocumentField('Photo'))
+    costs = db.ListField(db.EmbeddedDocumentField('Cost'))
+    sale_price = db.IntField()
+    created_by = db.EmbeddedDocumentField('User')
+    created_at = db.DateTimeField(default=datetime.datetime.now, required=True)
+    updated_by = db.EmbeddedDocumentField('User')
+    updated_at = db.DateTimeField()
+    status = db.EmbeddedDocumentField('TrackingStatus')
+    history = db.StringField()
+    note = db.StringField()
+    sold = db.BooleanField()
+    meta = {
+        'allow_inheritance': True,
+        'indexes': ['-created_at'],
+        'ordering': ['-created_at']
+    }
+
+    def change_status(self, to_status):
+        self.status = to_status
